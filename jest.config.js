e2432bb25d13037aba9a61c36ab3de8d1f2465ca
Alan <Alanpereira2@hotmail.com>
2025-05-09 01:07:02 -0300

    fix: removed babel from project

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.jest.json', // Arquivo específico para testes
+        isolatedModules: true, // Importante para performance
+      }
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Ajuste para seus aliases
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!.*\\.mjs$)', // Ignora node_modules, exceto .mjs
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
