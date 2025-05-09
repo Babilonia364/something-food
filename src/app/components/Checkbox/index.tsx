01ba70cf86c7598c67679f98eecca4c6268c4f19
Alan <Alanpereira2@hotmail.com>
2025-05-09 20:54:19 -0300

    feat(CHECKBOUX): used tv for future improvments

--- src/app/components/Checkbox/index.tsx
+++ src/app/components/Checkbox/index.tsx
@@ -5,6 +5,26 @@ import {
 } from '@radix-ui/react-checkbox';
 import check from '@/app/assets/check.svg';
 import Image from 'next/image';
+import { tv } from 'tailwind-variants';
+
+// Defining variants to be easier to implement future features
+const checkbox = tv({
+  slots: {
+    base: 'flex gap-2 items-center',
+    checkbox: 'flex size-[16px] appearance-none items-center justify-center rounded-sm outline-none border-2',
+    label: 'text-sm text-content-neutral-weak'
+  },
+  variants: {
+    state: {
+      default: {
+        checkbox: 'bg-surface-background border-content-neutral-weakest hover:bg-surface-middleground'
+      },
+    }
+  },
+  defaultVariants: {
+    state: 'default'
+  }
+});
 
 interface CheckboxProps {
   id: string;
@@ -16,19 +36,22 @@ type Products = {
 }
 
 export const Checkbox = ({ items }: Products) => {
+  const { base, checkbox: checkboxStyle, label } = checkbox();
 
   return (
-    items.map((item) =>
-      <div className='flex gap-2 items-center' key={item.id}>
-        <RadixCheckbox className="flex size-[16px] appearance-none items-center justify-center rounded-sm bg-surface-background outline-none border-2 border-content-neutral-weakest hover:bg-surface-middleground">
-          <RadixCheckboxIndicator>
-            <Image src={check} alt="check icon" />
-          </RadixCheckboxIndicator>
-        </RadixCheckbox>
-        <label className='text-sm text-content-neutral-weak'>
-          {item.label}
-        </label>
-      </div>
-    )
+    <>
+      {items.map((item) => (
+        <div className={base()} key={item.id}>
+          <RadixCheckbox className={checkboxStyle()}>
+            <RadixCheckboxIndicator>
+              <Image src={check} alt="check icon" />
+            </RadixCheckboxIndicator>
+          </RadixCheckbox>
+          <label className={label()}>
+            {item.label}
+          </label>
+        </div>
+      ))}
+    </>
   );
 };
