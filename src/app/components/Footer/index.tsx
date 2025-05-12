d37250424bc6c03c79482155b48607a56bbf55a3
Alan <Alanpereira2@hotmail.com>
2025-05-12 15:47:03 -0300

    feat: added navbar

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center justify-center h-[119px] bg-surface-foreground bottom-0">
+      <p className="text-sm text-content-primary-strong font-bold mb-1">feito com 💜 em maringá-PR</p>
+      <p className="text-base font-bold text-content-primary-strong">aiqfome.com © 2007-2023 aiqfome LTDA </p>
+      <p className="text-base font-bold text-content-primary-strong">CNPJ: 09.186.786/0001-58</p>
+    </footer>
+  );
+};
